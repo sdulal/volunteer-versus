ee1cdb79200faa70b7e3a54592903511dd9b1c5c
Shafqat Dulal <shafqat.dulal@gmail.com>
2015-06-08 15:07:00 -0700

    Change views for group asnd refactor some layout/user views with error messages

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,8 +1,18 @@
 class GroupsController < ApplicationController
   def new
+    @group = Group.new
   end
 
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      # flash that group has been created.
+      # Set up membership and admin for creator.
+      flash[:success] = "Group created!"
+      redirect_to group_url(@group)
+    else
+      redirect_to new_group_url
+    end
   end
 
   def update
@@ -15,8 +25,17 @@ class GroupsController < ApplicationController
   end
 
   def index
+    @groups = Group.all
   end
 
   def show
+    @group = Group.find(params[:id])
   end
+
+
+  private
+
+    def group_params
+      params.require(:group).permit(:name, :description)
+    end
 end
