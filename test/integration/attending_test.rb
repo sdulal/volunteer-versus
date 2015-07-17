c8bb8804f52fdf063cb1763b36714b391c89f280
Shafqat Dulal <shafqat.dulal@gmail.com>
2015-07-17 13:03:00 -0700

    Test the attendance index

--- test/integration/attending_test.rb
+++ test/integration/attending_test.rb
@@ -8,7 +8,21 @@ class EventsActionTest < ActionDispatch::IntegrationTest
     add_n_random_events_to(@group, 1)
     @event = @group.events.first
     @user.join(@group)
+    @group.promote_to_admin(@user)
     log_in_as(@user)
+  end
+
+  test "attendance page as admin" do
+    @user.attend(@event)
+    @event.update_attributes(date: Date.yesterday)
+    get event_attendances_path(@event)
+    assert_template 'attendances/index'
+    @event.attendees.each do |attendee|
+      attendance = attendee.attendance_for(@event)
+      assert_select "a[href=?]", user_path(attendee)
+      assert_select "a", text: "Check off"
+      assert_select "a[href=?]", edit_attendance_path(attendance)
+    end
   end
 
   test "attend an event the standard way" do
