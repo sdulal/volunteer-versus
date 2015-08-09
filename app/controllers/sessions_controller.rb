a99d92d870b38ac9d59b91d8a7c2bbe63adcb289
Shafqat Dulal <shafqat.dulal@gmail.com>
2015-08-08 19:12:52 -0700

    Enhance testing of the "remember me" ability
    The test previously checked for just the existence of the remember
    token in cookies. The changes made in the controller and the test
    mean that there is now a check for equality of the remember token
    across the different areas that it is stored in.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      if user.activated?
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      if @user.activated?
         # Log the user in and redirect to the user's show page.
-        log_in user
-        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-        redirect_back_or user
+        log_in @user
+        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+        redirect_back_or @user
       else
         message  = "Account not activated. "
         message += "Check your email for the activation link."
@@ -28,5 +28,5 @@ class SessionsController < ApplicationController
   def destroy
     log_out if logged_in?
     redirect_to root_url
-  end        
+  end
 end
