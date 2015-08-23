71e8aa3eb36c3b54624dbca564544c3d4ea32e0d
Shafqat Dulal <shafqat.dulal@gmail.com>
2015-08-22 20:10:47 -0700

    Refactor code for displaying dates, times and numbers
    This commit introduces standardization of how dates, times and numbers
    are displayed within the application. Tests already had helpers to
    enforce standards of display; the new changes carry over
    standardization via universally-accessed application helpers.
    
    The refactoring covers views related to the event and user models for
    now.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+  include ActionView::Helpers::NumberHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
@@ -13,5 +14,21 @@ module ApplicationHelper
   # Convenience method for getting by id.
   def by_id
     params[:id]
+  end
+
+  # Getting the formatted version that views of the application use for decimal
+  # numbers.
+  def formatted_number(number)
+    number_with_precision(number, precision: 2, strip_insignificant_zeros: true)
+  end
+
+  # Getting the formatted version that views of the application use for days.
+  def formatted_day(date)
+    date.strftime("%B %d, %Y")
+  end
+
+  # Getting the formatted version that views of the application use for times.
+  def formatted_time(time)
+    time.strftime("%l:%M %p")
   end
 end
