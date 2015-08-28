0b6af0b753d2b129c69932ebc53a0ae1680612ee
Shafqat Dulal <shafqat.dulal@gmail.com>
2015-08-28 13:10:05 -0700

    Comment docs for account activations controller

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,5 +1,6 @@
 class AccountActivationsController < ApplicationController
 
+  # Try to active account after user clicks link from email.
   def edit
     user = User.find_by(email: params[:email])
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
